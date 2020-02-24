0fa83cb83dc2a8bd58f982799c976461a6d14477
hadomanh <hadomanh99@gmail.com>
2020-02-24 14:38:22 +0700

    google auth

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,18 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const session = require('express-session');
-require('dotenv').config();
-const userModel=require('./user/user.schema');
-var Schema = mongoose.Schema;
 const bcryptjs = require('bcryptjs');
+const passport = require('passport');
+var Schema = mongoose.Schema;
+
+require('dotenv').config();
+
+
+const passportSetup = require('./passport/google-auth');
+const authRoutes = require('./routes/auth-routes');
+const userModel = require('./user/user.schema');
+
+
 // connect to mongodb
 mongoose.connect('mongodb://' + process.env.USER + ':' + process.env.PASS + '@localhost:27017/' + process.env.DATABASE + '?authSource=admin', { useNewUrlParser: true, useUnifiedTopology: true }, (e) => {
     //FIXME: tim cach viet khac
@@ -19,74 +27,81 @@ mongoose.connect('mongodb://' + process.env.USER + ':' + process.env.PASS + '@lo
         const server = express();
 
         server.use(express.static('public'));
+        // set up cors to allow us to accept requests from our client
         server.use(cors({
-            origin: ['http://localhost:3000'],
-            credentials: true
-        }));
+                origin: "http://localhost:3000", // allow to server to accept request from different origin
+                methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+                credentials: true // allow session cookie from browser to pass through
+            })
+        );
 
 
-        server.use(function (req, res, next) {
+        // server.use(function (req, res, next) {
 
-            // Website you wish to allow to connect
-            res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+        //     // Website you wish to allow to connect
+        //     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
-            // Request methods you wish to allow
-            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        //     // Request methods you wish to allow
+        //     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-            // Request headers you wish to allow
-            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        //     // Request headers you wish to allow
+        //     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-            // Set to true if you need the website to include cookies in the requests sent
-            // to the API (e.g. in case you use sessions)
-            res.setHeader('Access-Control-Allow-Credentials', true);
+        //     // Set to true if you need the website to include cookies in the requests sent
+        //     // to the API (e.g. in case you use sessions)
+        //     res.setHeader('Access-Control-Allow-Credentials', true);
 
-            // Pass to next layer of middleware
-            next();
-        });
+        //     // Pass to next layer of middleware
+        //     next();
+        // });
 
         server.use(bodyParser.json());
+
+        // set up session cookies
         server.use(session({
             secret: 'keyboard cat',
             resave: true,
             saveUninitialized: false,
         }));
 
-        server.get('/users', (req, res) => {
-            // var users = mongoose.model('users', new Schema());
+        // initialize passport
+        server.use(passport.initialize());
+        server.use(passport.session());
 
+        // set up auth route
+        server.use('/auth', authRoutes);
+
+        server.get('/users', (req, res) => {
             userModel.find({}, (err, data) => {
-                console.log(data);
                 res.send(data)
             })
-
-
         })
 
         server.post('/users', (req, res) => {
             console.log("new user: ", req.body.email);
-            const email=req.body.email;
-            const pass=req.body.password;
-            const name=req.body.name;
-            userModel.find({email:req.body.email},(err,data)=>{
-                if(data.length>0) {
+            const email = req.body.email;
+            const pass = req.body.password;
+            const name = req.body.name;
+            userModel.find({ email: req.body.email }, (err, data) => {
+                if (data.length > 0) {
                     console.log("email exist");
                     res.status(400).json({
                         success: false,
                         message: "Email has been used"
                     });
                 }
-                else{
+                else {
                     const hashPassword = bcryptjs.hashSync(pass, 10);
-                    
+
                     userModel.create({
                         email: email,
                         password: hashPassword,
                         name: name
                     })
-                    
+
                     res.status(201).json({
                         success: true,
-                        message: "Account " + email +" has been created"
+                        message: "Account " + email + " has been created"
                     });
                 }
             })
