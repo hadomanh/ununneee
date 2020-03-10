0dbef1d3afe6578032a1719d5e6f3b4900d7995a
tanhng <tienanh240599@gmail.com>
2020-03-10 11:06:52 +0700

    create upload Ava route

--- backend/user/user.routes.js
+++ backend/user/user.routes.js
@@ -1,39 +1,13 @@
 const route = require('express').Router();
 const userModel = require('./user.schema');
-const bcryptjs= require('bcryptjs');
-const session = require('express-session');
-const cors = require('cors');
-
-
-
-route.use(cors({
-    origin: "http://localhost:3000", // allow to server to accept request from different origin
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true // allow session cookie from browser to pass through
-})
-);
-route.use(session({
-    secret: 'keyboard cat',
-    // resave: true,
-    // saveUninitialized: false,
-    // cookie: { secure: true }
-}));
+const bcryptjs = require('bcryptjs');
 route.get('/', (req, res) => {
-    console.log('session ne',req.session);
-    res.status(500).json({
-        success: true,
-    });
+    console.log('session ne', req.session);
 })
 
 
-
-route.get("/test",(req,res)=>{
-    console.log('session ne',req.session);
-    res.send(req.session);
-})
-
-route.get('/get-fighter-by-id/:id',(req,res)=>{
-    console.log('fightId ne',req.params.id);
+route.get('/get-fighter-by-id', (req, res) => {
+    console.log('fightId ne', req.query.fighterId);
 })
 
 
@@ -62,15 +36,13 @@ route.post('/verify', (req, res) => {
                     req.session.currentUser = {
                         email: data.email,
                     };
-                    
                     console.log(req.session);
                     res.status(200).json({
                         success: true,
                         message: "Login Success",
                         data: {
                             email: data.email,
-                            name: data.name,
-                            id:data.id,
+                            name: data.name
                         }
                     })
                 }
@@ -86,41 +58,51 @@ route.post('/verify', (req, res) => {
     })
 })
 
-route.post('/', (req, res) => {
-    const email = req.body.email;
-    const pass = req.body.password;
-    const name = req.body.name;
-    userModel.findOne({ email: req.body.email }, (err, data) => {
-        if (data !== null) {
-            res.status(400).json({
-                success: false,
-                message: "Email has been used"
-            });
-        }
-        else {
-            const hashPassword = bcryptjs.hashSync(pass, 10);
 
-            userModel.create({
-                email: email,
-                password: hashPassword,
-                name: name,
-            });
-            req.session.currentUser = {
-                email: req.body.email,
-            };
-            res.status(201).json({
-                success: true,
-                message: "Account " + email + " has been created",
-                data: {
-                    name: req.body.name,
-                    email: req.body.email,
-                }
-            });
-        }
+route.post('/uploadAva', (req, res) => {
+    console.log('upload avatar req.session', req.session);
+    res.status(200).json({
+        success: true,
     })
 })
 
-module.exports = route;
+
+
+    route.post('/', (req, res) => {
+        const email = req.body.email;
+        const pass = req.body.password;
+        const name = req.body.name;
+        userModel.findOne({ email: req.body.email }, (err, data) => {
+            if (data !== null) {
+                res.status(400).json({
+                    success: false,
+                    message: "Email has been used"
+                });
+            }
+            else {
+                const hashPassword = bcryptjs.hashSync(pass, 10);
+
+                userModel.create({
+                    email: email,
+                    password: hashPassword,
+                    name: name,
+                });
+                req.session.currentUser = {
+                    email: req.body.email,
+                };
+                res.status(201).json({
+                    success: true,
+                    message: "Account " + email + " has been created",
+                    data: {
+                        name: req.body.name,
+                        email: req.body.email,
+                    }
+                });
+            }
+        })
+    })
+
+    module.exports = route;
 
 
 // get /user/:id -- lay 1 thang
